372996335ed7cf30084cbe33a71882f93efd0434
JavichDev31 <javichtech@gmail.com>
2026-05-10 10:22:54 -0400

    Update AssemblyInfo: Replace BobikA5 references with JavichRamdiskA5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,13 +4,13 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyTitle("BobikA5")]
-[assembly: AssemblyDescription("BobikA5")]
+[assembly: AssemblyTitle("JavichRamdiskA5")]
+[assembly: AssemblyDescription("JavichRamdiskA5 - Modified version")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("@Pkkf5673")]
-[assembly: AssemblyProduct("BobikA5")]
+[assembly: AssemblyCompany("@JavichDev31")]
+[assembly: AssemblyProduct("JavichRamdiskA5")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
-[assembly: AssemblyTrademark("BobikA5")]
+[assembly: AssemblyTrademark("JavichRamdiskA5")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8652cd96-38e9-4f0b-a8ae-d0c5e1af1a7e")]
 [assembly: AssemblyFileVersion("1.1.1")]
